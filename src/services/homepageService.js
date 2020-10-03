dda3acabadd3a88ad946d002f8adf8bdac346ffc
Hnin Htet Htet Htoo <hninhtethtethtoo@gmail.com>
2020-10-03 22:10:45 +0630

    edit persistence menu with nested for inserting more than 3 infos

--- src/services/homepageService.js
+++ src/services/homepageService.js
@@ -32,10 +32,22 @@ let handleSetupProfileAPI = () => {
                                 "payload": "CURATION"
                             },
                             {
-                                "type": "web_url",
-                                "title": "View facebook fan page",
-                                "url": "https://www.facebook.com/techShopHaryphamdev",
-                                "webview_height_ratio": "full"
+                                "type": "nested",
+                                "title": "More_Info",
+                                "call_to_actions":[
+                                    {
+                                        "type": "web_url",
+                                        "title": "View facebook fan page",
+                                        "url": "https://www.facebook.com/techShopHaryphamdev",
+                                        "webview_height_ratio": "full"
+                                    },
+                                    {
+                                        "type": "web_url",
+                                        "title": "View youtube channel",
+                                        "url": "https://bit.ly/subscribe-haryphamdev",
+                                        "webview_height_ratio": "full"
+                                    }
+                                ]
                             }
                         ]
                     }

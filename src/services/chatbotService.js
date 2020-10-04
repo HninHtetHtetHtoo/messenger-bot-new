5a0edc6ab9a8ff84be5f9226aa2ecbf092c76c76
Hnin Htet Htet Htoo <hninhtethtethtoo@gmail.com>
2020-10-04 22:39:06 +0630

    update target_app_id

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -4,6 +4,7 @@ import request from 'request';
 import homepageService from "./homepageService";
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const SECONDARY_RECEIVER_ID = process.env.SECONDARY_RECEIVER_ID;
 
 let sendMessage = (sender_psid, response) => {
     return new Promise(async (resolve, reject) => {
@@ -252,7 +253,7 @@ let passThreadControl = (sender_psid) => {
                 "recipient": {
                     "id": sender_psid
                 },
-                "target_app_id": process.env.SECONDARY_RECEIVER_ID,
+                "target_app_id": SECONDARY_RECEIVER_ID,
                 "metadata": "Pass thread control to inbox chat."
             };
 
